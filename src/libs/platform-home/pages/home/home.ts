1d337460fa78f342ec5cf4497872c67187ed9ccb
李晨熙 <licx@ACA80341.ipt.aol.com>
2018-02-07 10:35:53 +0800

    首页样式

--- src/libs/platform-home/pages/home/home.ts
+++ src/libs/platform-home/pages/home/home.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../platform-core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'page-platform-home',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-
-  constructor() { }
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {}
+
+  // 退出登录
+  logout(): void {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }
